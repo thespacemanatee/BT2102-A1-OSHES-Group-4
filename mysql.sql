24aa53a4f30add6defd43e50bd74fc203b6ff6d5
Chee Kit <cheekit.chong98@gmail.com>
2021-10-06 21:09:11 +0800

    Fix and format SQL

--- mysql.sql
+++ mysql.sql
@@ -1,21 +1,20 @@
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`db.OSHES` /*!40100 DEFAULT CHARACTER SET latin1 */;
+DROP DATABASE `db.OSHES`;
+
+CREATE DATABASE IF NOT EXISTS `db.OSHES`;
 
 USE `db.OSHES`;
 
-
-/*table structure for `CUSTOMER`*/
-DROP TABLE IF EXISTS `customer`;
-
-CREATE TABLE `customer` (
-`customerID` int AUTO_INCREMENT,
-`customerName` varchar(50) NOT NULL,
-`customerGender` char(1) NOT NULL,
-`email` varchar(100) NOT NULL,
-`address` varchar(50) NOT NULL,
-`customerPhone` int(10) NOT NULL,
-`customerPassword` varchar(50) NOT NULL,
-PRIMARY KEY (`customerID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE `customer`
+(
+    `id`       int         NOT NULL AUTO_INCREMENT,
+    `name`     varchar(50) NOT NULL,
+    `gender`   char(1)     NOT NULL,
+    `email`    varchar(50) NOT NULL,
+    `address`  varchar(50) NOT NULL,
+    `phone`    int(10)     NOT NULL,
+    `password` varchar(50) NOT NULL,
+    PRIMARY KEY (`id`)
+);
 
 /*test data for the table `customer`*/
 /*insert into `customer`(`customerID`, `customerName`, `customerGender`, `email`, `address`, `customerPhone`, `customerPassword`) values
@@ -23,21 +22,15 @@ PRIMARY KEY (`customerID`)
 (2, 'Ben', 'M', 'ben@bt.com', 'sg', 1234567809, 'pass2'),
 (3, 'Carl', 'M', 'carl@bt.com', 'sg', 1234567980, 'pass3');*/
 
-
-
-
-
-/*table structure for table `ADMINISTRATOR` */
-DROP TABLE IF EXISTS `administrator`;
-
-CREATE TABLE `administrator` (
-`adminID` int AUTO_INCREMENT,
-`adminName` varchar(50) NOT NULL,
-`adminGender` char(1) NOT NULL,
-`adminPhone` int(10) NOT NULL,
-`adminPassword` varchar(50) NOT NULL,
-PRIMARY KEY (`adminID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE `administrator`
+(
+    `id`       int         NOT NULL AUTO_INCREMENT,
+    `name`     varchar(50) NOT NULL,
+    `gender`   char(1)     NOT NULL,
+    `phone`    int(10)     NOT NULL,
+    `password` varchar(50) NOT NULL,
+    PRIMARY KEY (`id`)
+);
 
 /*test data for the table `administrator*/
 /*insert into `administrator`(`adminID`, `adminName`, `adminGender`, `adminPhone`, `adminPassword`) values
@@ -45,75 +38,55 @@ PRIMARY KEY (`adminID`)
 (5, 'Ferb', 'M', 1234576890, 'pass5'),
 (6, 'Greg', 'M', 1234657890, 'pass6');*/
 
-
-
-
-
-/*table structure for table `REQUEST`*/
-DROP TABLE IF EXISTS `request`;
-
-CREATE TABLE `request` (
-`requestID` int AUTO_INCREMENT,
-`serviceAmount` decimal(10,2) DEFAULT NULL,
-`servicePaymentDate` date DEFAULT NULL,
-`requestStatus` varchar(50) DEFAULT '',
-`requestDate` date DEFAULT NULL,
-`adminID` int DEFAULT NULL,
-PRIMARY KEY (`requestID`),
-CONSTRAINT `request_ibfk_1` FOREIGN KEY (`adminID`) REFERENCES `administrator` (`adminID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE `request`
+(
+    `id`                   int NOT NULL AUTO_INCREMENT,
+    `service_amount`       decimal(10, 2) DEFAULT NULL,
+    `service_payment_date` date           DEFAULT NULL,
+    `request_status`       varchar(50)    DEFAULT '',
+    `request_date`         date           DEFAULT NULL,
+    `admin_id`             int            DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    CONSTRAINT `request_ibfk_1` FOREIGN KEY (`admin_id`) REFERENCES `administrator` (`id`)
+);
 
 /*test data for the table `request` */
 /*insert into `request` (`customerID`, `requestID`, `serviceAmount`, `servicePaymentDate`, `requestStatus`, `requestDate`, `adminID`) values
 ('id1', 001, 0.00, '13-01-21', 'submitted', '12-01-21', 'id6');*/
 
-
-
-
-/* table structure for `PRODUCT`*/
-DROP TABLE IF EXISTS `product`;
-
-CREATE TABLE `product` (
-`productID` int UNIQUE,
-`category` varchar(50) NOT NULL,
-`model` varchar(50) NOT NULL,
-`cost` decimal(10,2) NOT NULL,
-`price` decimal(10,2) NOT NULL,
-`warranty` int NOT NULL,
-PRIMARY KEY (`productID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE `product`
+(
+    `id`       int            NOT NULL,
+    `category` varchar(50)    NOT NULL,
+    `model`    varchar(50)    NOT NULL,
+    `cost`     decimal(10, 2) NOT NULL,
+    `price`    decimal(10, 2) NOT NULL,
+    `warranty` int            NOT NULL,
+    PRIMARY KEY (`id`)
+);
 
 /*test data for the table `product`*/
 /*insert into `product`(`productID`, `category`, `model`, `cost`, `price`, `warranty`) values
 (1, 'light', 'safehome', 89.00, 120.00, 6);*/
 
-
-
-
-
-/*table stucture for table `ITEM`*/
-DROP TABLE IF EXISTS `item`;
-
-CREATE TABLE `item` (
-`itemID` int(4) NOT NULL UNIQUE,
-`colour` varchar(50) NOT NULL,
-`powerSupply` varchar(50) NOT NULL,
-`factory` varchar(50) NOT NULL,
-`productionYear` int NOT NULL,
-`purchaseStatus` varchar(6) NOT NULL,
-`serviceStatus` varchar(50) DEFAULT '',
-`purchaseDate` date DEFAULT NULL,
-`customerID` int DEFAULT NULL,
-`adminID` int DEFAULT NULL,
-`productID` int NOT NULL,
-PRIMARY KEY (`itemID`),
-KEY `customerID` (`customerID`),
-KEY `adminID` (`adminID`),
-KEY `productID` (`productID`),
-CONSTRAINT `item_ibfk_1` FOREIGN KEY (`customerID`) REFERENCES `customer`(`customerID`),
-CONSTRAINT `item_ibfk_2` FOREIGN KEY (`adminID`) REFERENCES `administrator`(`adminID`),
-CONSTRAINT `item_ibfk_3` FOREIGN KEY (`productID`) REFERENCES `product`(`productID`) 
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE `item`
+(
+    `id`              int         NOT NULL,
+    `colour`          varchar(50) NOT NULL,
+    `power_supply`    varchar(50) NOT NULL,
+    `factory`         varchar(50) NOT NULL,
+    `production_year` int         NOT NULL,
+    `purchase_status` varchar(6)  NOT NULL,
+    `service_status`  varchar(50) DEFAULT '',
+    `purchase_date`   date        DEFAULT NULL,
+    `customer_id`     int         DEFAULT NULL,
+    `admin_id`        int         DEFAULT NULL,
+    `product_id`      int         NOT NULL,
+    PRIMARY KEY (`id`),
+    CONSTRAINT `item_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`id`),
+    CONSTRAINT `item_ibfk_2` FOREIGN KEY (`admin_id`) REFERENCES `administrator` (`id`),
+    CONSTRAINT `item_ibfk_3` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`)
+);
 
 /*test data for the table `item`*/
 /*insert into `item`(`itemID`, `colour`, `powerSupply`, `factory`, `productionYear`, `purchaseStatus`, `serviceStatus`, `purchaseDate`, `customerID`, `adminID`, `productID`) values
