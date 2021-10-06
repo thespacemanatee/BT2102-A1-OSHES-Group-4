cb74587ab1cd0d9679aa9d2dc653ced3a5e08982
Chee Kit <cheekit.chong98@gmail.com>
2021-10-06 12:51:15 +0800

    Test MySQL connection

--- app/database/setup.py
+++ app/database/setup.py
@@ -1,8 +1,22 @@
 from pymongo import MongoClient
+import mysql.connector
 
 MONGODB_URL = 'mongodb://localhost:27017'
 
-client = MongoClient(MONGODB_URL)
-db = client.OSHES
+mongo_client = MongoClient(MONGODB_URL)
+db = mongo_client.OSHES
 Products = db.Products
 Items = db.Items
+
+mysql_client = mysql.connector.connect(
+    host="localhost",
+    user='root',
+    password='password'
+)
+
+mycursor = mysql_client.cursor()
+
+mycursor.execute("SHOW DATABASES")
+
+for x in mycursor:
+    print(x)
