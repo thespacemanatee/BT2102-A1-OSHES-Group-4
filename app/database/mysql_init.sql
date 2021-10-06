05bdc6db7cdae6ae8e1b043819019833f15c3504
Chee Kit <cheekit.chong98@gmail.com>
2021-10-07 03:10:53 +0800

    Format and remove old comments

--- app/database/mysql_init.sql
+++ app/database/mysql_init.sql
@@ -16,12 +16,6 @@ CREATE TABLE `customer`
     PRIMARY KEY (`id`)
 );
 
-/*test data for the table `customer`*/
-/*insert into `customer`(`customerID`, `customerName`, `customerGender`, `email`, `address`, `customerPhone`, `customerPassword`) values
-(1, 'Aly', 'F', 'aly@bt.com', 'sg', 1234567890, 'pass1'), 
-(2, 'Ben', 'M', 'ben@bt.com', 'sg', 1234567809, 'pass2'),
-(3, 'Carl', 'M', 'carl@bt.com', 'sg', 1234567980, 'pass3');*/
-
 CREATE TABLE `administrator`
 (
     `id`       varchar(50) NOT NULL,
@@ -32,29 +26,19 @@ CREATE TABLE `administrator`
     PRIMARY KEY (`id`)
 );
 
-/*test data for the table `administrator*/
-/*insert into `administrator`(`adminID`, `adminName`, `adminGender`, `adminPhone`, `adminPassword`) values
-(4, 'Dina', 'F', 1234568790, 'pass4'),
-(5, 'Ferb', 'M', 1234576890, 'pass5'),
-(6, 'Greg', 'M', 1234657890, 'pass6');*/
-
 CREATE TABLE `request`
 (
-    `id`                   int            NOT NULL AUTO_INCREMENT,
+    `id`                   int         NOT NULL AUTO_INCREMENT,
     `service_amount`       decimal(10, 2) DEFAULT NULL,
     `service_payment_date` date           DEFAULT NULL,
-    `request_status`       varchar(50)    NOT NULL,
-    `request_date`         date           NOT NULL,
+    `request_status`       varchar(50) NOT NULL,
+    `request_date`         date        NOT NULL,
     `customer_id`          varchar(50)    DEFAULT NULL,
     `admin_id`             varchar(50)    DEFAULT NULL,
     PRIMARY KEY (`id`),
     CONSTRAINT `request_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`id`),
     CONSTRAINT `request_ibfk_2` FOREIGN KEY (`admin_id`) REFERENCES `administrator` (`id`)
 );
-
-/*test data for the table `request` */
-/*insert into `request` (`customerID`, `requestID`, `serviceAmount`, `servicePaymentDate`, `requestStatus`, `requestDate`, `adminID`) values
-('id1', 001, 0.00, '13-01-21', 'submitted', '12-01-21', 'id6');*/
 
 CREATE TABLE `product`
 (
@@ -66,10 +50,6 @@ CREATE TABLE `product`
     `warranty` int            NOT NULL,
     PRIMARY KEY (`id`)
 );
-
-/*test data for the table `product`*/
-/*insert into `product`(`productID`, `category`, `model`, `cost`, `price`, `warranty`) values
-(1, 'light', 'safehome', 89.00, 120.00, 6);*/
 
 CREATE TABLE `item`
 (
@@ -89,11 +69,3 @@ CREATE TABLE `item`
     CONSTRAINT `item_ibfk_2` FOREIGN KEY (`admin_id`) REFERENCES `administrator` (`id`),
     CONSTRAINT `item_ibfk_3` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`)
 );
-
-/*test data for the table `item`*/
-/*insert into `item`(`itemID`, `colour`, `powerSupply`, `factory`, `productionYear`, `purchaseStatus`, `serviceStatus`, `purchaseDate`, `customerID`, `adminID`, `productID`) values
-('1001', 'black', 'battery', 'malaysia', 2014, 'sold', '', '23-01-21', 'id1', 'id4', 1);*/
-
-
-
-
